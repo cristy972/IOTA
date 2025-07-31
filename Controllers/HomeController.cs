bf51c47980a4d7358dbd6d89f13fc345b7c1406b
Dan Cristian Oita <cristy97cristy@gmail.com>
2025-07-31 21:40:29 +0300

    added parameters controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using IOTA.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.Extensions.Logging;
 
 namespace IOTA.Controllers
 {
---
+++ Controllers/ParametersManagementController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+
+namespace IOTA.Controllers
+{
+    public class ParametersManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
