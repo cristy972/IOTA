641aba9ba223115449d391e9e7ccab67f6141c93
Dan Cristian Oita <cristy97cristy@gmail.com>
2025-07-31 20:58:37 +0300

    add bin to gitignore

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace IOTA.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
